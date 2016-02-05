051df63e96aae54ca1df7181602ee525e4570db9
Teodor Kostadinov <teodor.kk.93@gmail.com>
2016-02-05 23:05:16 +0200

    Added toolbar with actions

--- app/src/main/java/com/example/admin/twitterdemo/BaseActivity.java
+++ app/src/main/java/com/example/admin/twitterdemo/BaseActivity.java
@@ -2,12 +2,19 @@ package com.example.admin.twitterdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class BaseActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +26,23 @@ public class BaseActivity extends AppCompatActivity {
         ViewGroup container = (ViewGroup) findViewById(R.id.container);
         getLayoutInflater().inflate(resource, container, true);
         ButterKnife.bind(this);
+        initToolbar();
         initFields();
         initViews();
         initListeners();
+    }
+
+    private void initToolbar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setIcon(R.drawable.logo_hdpi);
+        getSupportActionBar().setTitle("Twitter");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     protected void initFields() {
